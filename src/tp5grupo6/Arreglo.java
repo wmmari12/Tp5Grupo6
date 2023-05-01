867cffe4c8ab5929bf3020c3456f001fb4def369
wmmari12 <wmari12@gmail.com>
2023-05-01 20:51:00 -0300

    Initial commit

---
+++ src/tp5grupo6/Arreglo.java
@@ -0,0 +1,60 @@
+
+package tp5grupo6;
+
+
+public class Arreglo {
+    
+   
+    
+    public static void  sumarLista(int arreglo[]){
+        
+        int suma=0;
+        for (int i = 0; i < arreglo.length; i++)
+        {
+            suma+=arreglo[i];
+        }
+        System.out.println("La suma de los elementos es: "+suma);
+        System.out.println("El promedio de los elementos es: "+(suma/arreglo.length));
+    }
+    
+    public static void  sumarMayor(int arreglo[][]){
+        
+        int mayor=arreglo[0][0];
+        for (int i = 0; i < arreglo.length; i++)
+        {
+            
+            for (int j = 0; j < arreglo[i].length; j++)
+            {
+                if(mayor<arreglo[i][j]){
+                    mayor=arreglo[i][j];
+                }
+            }
+        }
+        System.out.println("el mayor numero ingresado es: "+mayor);
+    }
+    
+    public static void cuentaVocales(String palabra){
+        int contador=0;
+        for (int i = 0; i < palabra.length(); i++)
+        {
+            char a=palabra.charAt(i);
+            if(a=='a'||a=='e'||a=='i'|| a=='o'||a=='u'){
+               contador+=1;
+            }
+        }
+        System.out.println("La palabra ingresada tiene "+contador+" vocales.");
+    }
+    
+    public static void cuentaCaracter(String palabra,char letra){
+        int contador=0;
+        for (int i = 0; i < palabra.length(); i++)
+        {
+            char a=palabra.charAt(i);
+            if(a==letra){
+               contador+=1;
+            }
+        }
+        System.out.println("La palabra tiene "+contador+" letras "+letra);
+    }
+    
+}
---
+++ src/tp5grupo6/Tp5Grupo6.java
@@ -0,0 +1,20 @@
+
+package tp5grupo6;
+
+
+public class Tp5Grupo6 {
+
+    
+    public static void main(String[] args) {
+        
+         int [] arreglo={1,2,3,4,5,6,7,8,9,10};
+         int [][] arreglo1={{1,2,3},{4,20},{6,7,8}};
+         String palabra="hola mundo";
+         
+         Arreglo.sumarLista(arreglo);
+         Arreglo.sumarMayor(arreglo1);
+         Arreglo.cuentaVocales(palabra);
+         Arreglo.cuentaCaracter(palabra, 'o');
+    }
+    
+}
